a2854ff03570d0304b5386bfe155e36690cbd9bc
ArmanShaikh05 <arman.shaikh01082003@gmail.com>
2025-06-09 23:11:08 +0530

    model testing 1

--- components/inventory/CreateNewBrand.tsx
+++ components/inventory/CreateNewBrand.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { createNewBrand, deleteBrand } from "@/actions/utilityActions";
 import {
   AlertDialog,
@@ -18,89 +19,89 @@ import { toast } from "sonner";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
-import { ScrollArea } from "../ui/scroll-area";
+// import { ScrollArea } from "../ui/scroll-area";
 
-const BrandCard = ({ brandData }: { brandData: SerializedBrandType }) => {
-  const router = useRouter();
-  const [loading, setLoading] = useState<boolean>(false);
-  const [showDeleteBrandDialog, setShowDeleteBrandDialog] =
-    useState<boolean>(false);
+// const BrandCard = ({ brandData }: { brandData: SerializedBrandType }) => {
+//   const router = useRouter();
+//   const [loading, setLoading] = useState<boolean>(false);
+//   const [showDeleteBrandDialog, setShowDeleteBrandDialog] =
+//     useState<boolean>(false);
 
-  const handleDeleteBrand = async (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    try {
-      e.preventDefault();
-      setLoading(true);
+//   const handleDeleteBrand = async (
+//     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+//   ) => {
+//     try {
+//       e.preventDefault();
+//       setLoading(true);
 
-      const response: { success: boolean; message: string } = await deleteBrand(
-        brandData.id
-      );
+//       const response: { success: boolean; message: string } = await deleteBrand(
+//         brandData.id
+//       );
 
-      if (response.success) {
-        toast.success(response.message);
-        router.refresh();
-        setShowDeleteBrandDialog(false);
-      }
+//       if (response.success) {
+//         toast.success(response.message);
+//         router.refresh();
+//         setShowDeleteBrandDialog(false);
+//       }
 
-      if (!response.success) {
-        toast.error(response.message);
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+//       if (!response.success) {
+//         toast.error(response.message);
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
 
-  return (
-    <div className="w-full px-6 py-2  border-b flex justify-between items-center">
-      <div className="flex flex-col grow">
-        <h2 className="text-sm">{brandData.brandName}</h2>
-        <p className="text-[10px]">{brandData.totalProduct} products</p>
-      </div>
+//   return (
+//     <div className="w-full px-6 py-2  border-b flex justify-between items-center">
+//       <div className="flex flex-col grow">
+//         <h2 className="text-sm">{brandData.brandName}</h2>
+//         <p className="text-[10px]">{brandData.totalProduct} products</p>
+//       </div>
 
-      <AlertDialog
-        open={showDeleteBrandDialog}
-        onOpenChange={setShowDeleteBrandDialog}
-      >
-        <AlertDialogTrigger asChild>
-          {brandData.totalProduct === 0 && (
-            <div className="aspect-square w-8 flex justify-center items-center rounded-full hover:bg-main/20 transition duration-300 cursor-pointer">
-              <Trash2 size={16} />
-            </div>
-          )}
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>
-              Do you want to delete <b>{brandData.brandName}</b> Brand?
-            </AlertDialogTitle>
-            <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete the
-              Brand.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction asChild>
-              <Button disabled={loading} onClick={(e) => handleDeleteBrand(e)}>
-                {loading ? (
-                  <div className="flex w-full items-center justify-center gap-2">
-                    <Loader2 className="w-4 h-4 animate-spin" />
-                    <span className="text-sm">Deleting Brand</span>
-                  </div>
-                ) : (
-                  "Delete Brand"
-                )}
-              </Button>
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    </div>
-  );
-};
+//       <AlertDialog
+//         open={showDeleteBrandDialog}
+//         onOpenChange={setShowDeleteBrandDialog}
+//       >
+//         <AlertDialogTrigger asChild>
+//           {brandData.totalProduct === 0 && (
+//             <div className="aspect-square w-8 flex justify-center items-center rounded-full hover:bg-main/20 transition duration-300 cursor-pointer">
+//               <Trash2 size={16} />
+//             </div>
+//           )}
+//         </AlertDialogTrigger>
+//         <AlertDialogContent>
+//           <AlertDialogHeader>
+//             <AlertDialogTitle>
+//               Do you want to delete <b>{brandData.brandName}</b> Brand?
+//             </AlertDialogTitle>
+//             <AlertDialogDescription>
+//               This action cannot be undone. This will permanently delete the
+//               Brand.
+//             </AlertDialogDescription>
+//           </AlertDialogHeader>
+//           <AlertDialogFooter>
+//             <AlertDialogCancel>Cancel</AlertDialogCancel>
+//             <AlertDialogAction asChild>
+//               <Button disabled={loading} onClick={(e) => handleDeleteBrand(e)}>
+//                 {loading ? (
+//                   <div className="flex w-full items-center justify-center gap-2">
+//                     <Loader2 className="w-4 h-4 animate-spin" />
+//                     <span className="text-sm">Deleting Brand</span>
+//                   </div>
+//                 ) : (
+//                   "Delete Brand"
+//                 )}
+//               </Button>
+//             </AlertDialogAction>
+//           </AlertDialogFooter>
+//         </AlertDialogContent>
+//       </AlertDialog>
+//     </div>
+//   );
+// };
 
 const CreateNewBrand = ({
   brandsData,
@@ -189,7 +190,77 @@ const CreateNewBrand = ({
 
   return (
     <>
-      <AlertDialog>
+      <AlertDialog
+        open={showCreateNewBrandDialog}
+        onOpenChange={setShowCreateNewBrandDialog}
+      >
+        <AlertDialogTrigger asChild>
+          <Button className="w-full text-xs h-8 mt-2 flex items-center justify-center gap-2">
+            <Plus size={14} />
+            New Brand
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Create Brand</AlertDialogTitle>
+            <AlertDialogDescription></AlertDialogDescription>
+          </AlertDialogHeader>
+
+          <div className="grid gap-6">
+            <div className="grid gap-3">
+              <Label htmlFor="name-1">Brand Name</Label>
+              <Input
+                id="name-1"
+                value={brandName}
+                onChange={(e) => setBrandName(e.target.value)}
+                placeholder="Brand name here"
+              />
+              {errorStates.nameError && (
+                <p className="text-red-500 text-xs mt-2">
+                  {errorStates.nameError}
+                </p>
+              )}
+            </div>
+            <div className="grid gap-3">
+              <Label>Color code</Label>
+              <div className="w-full flex items-center gap-2 ">
+                {colors.map((color, index) => (
+                  <span
+                    key={index}
+                    className={`h-6 w-6 rounded-full cursor-pointer ${color} ${
+                      color.includes(colorCode) && "border-3 border-black"
+                    }`}
+                    onClick={() => setColorCode(color.slice(4, 11))}
+                  />
+                ))}
+              </div>
+              {errorStates.colorError && (
+                <p className="text-red-500 text-xs mt-2">
+                  {errorStates.colorError}
+                </p>
+              )}
+            </div>
+          </div>
+
+          <AlertDialogFooter>
+            <AlertDialogCancel disabled={loading}>Cancel</AlertDialogCancel>
+            <AlertDialogAction asChild>
+              <Button disabled={loading} onClick={(e) => createBrand(e)}>
+                {loading ? (
+                  <div className="flex w-full items-center justify-center gap-2">
+                    <Loader2 className="w-4 h-4 animate-spin" />
+                    <span className="text-sm">Creating Brand</span>
+                  </div>
+                ) : (
+                  "Create Brand"
+                )}
+              </Button>
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      {/* <AlertDialog>
         <AlertDialogTrigger asChild>
           <Button className="w-full text-xs h-8 mt-2 flex items-center justify-center gap-2">
             <Plus size={14} />
@@ -224,9 +295,9 @@ const CreateNewBrand = ({
             </AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
-      </AlertDialog>
+      </AlertDialog> */}
 
-      {showCreateNewBrandDialog && (
+      {/* {showCreateNewBrandDialog && (
         <AlertDialog
           open={showCreateNewBrandDialog}
           onOpenChange={setShowCreateNewBrandDialog}
@@ -290,7 +361,7 @@ const CreateNewBrand = ({
             </AlertDialogFooter>
           </AlertDialogContent>
         </AlertDialog>
-      )}
+      )} */}
     </>
   );
 };
